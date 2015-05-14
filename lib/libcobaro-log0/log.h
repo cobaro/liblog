2b1b486251ba19322569511d1a2bd6ab8fe55dc4
David Arnold <d@0x1.org>
2015-05-14 03:37:20 +0000

    Tidy doxygen.
    Consistent indents in header.
    Consistent spacing of parameter documentation.
    Consistent naming of things: "Log handle", "log structure", etc.
    Full stops at ends of things.
    Mark NULL, true and false as code.
    Add @ref's where they seem to be required.
    Reformat enum docs to encourage doxygen to actually pick it up.
    Minor rewording here and there.

--- lib/libcobaro-log0/log.h
+++ lib/libcobaro-log0/log.h
@@ -23,48 +23,24 @@ COPYRIGHT_END
 
 /// Type discriminator for parameter values in logs.
 enum cobaro_log_types {
-    /// @var Value is a string.
-    COBARO_STRING = 1,
-
-    /// @var Value is a signed integer.
-    COBARO_INTEGER = 2,
-
-    /// @var Value is a double.
-    COBARO_REAL = 3,
-
-    /// @var Value is an IP address (in 32 bit network format).
-    COBARO_IPV4 = 4
+    COBARO_STRING = 1,  ///< Value is a string.
+    COBARO_INTEGER = 2,  ///< Value is a signed integer.
+    COBARO_REAL = 3,  ///< Value is a double.
+    COBARO_IPV4 = 4  ///< Value is an IP address (in 32 bit network format).
 };
 
 
-/// Loglevels as they come from syslog.
+/// Log levels as they come from syslog(3).
 enum cobaro_log_levels {
-    /// @var System is unusable.
-    COBARO_LOG_EMERG = 0,
-
-    /// @var Action must be taken immediately.
-    COBARO_LOG_ALERT,
-
-    /// @var Critical conditions.
-    COBARO_LOG_CRIT,
-
-    /// @var Error conditions.
-    COBARO_LOG_ERR,
-
-    /// @var Warning conditions.
-    COBARO_LOG_WARNING,
-
-    /// @var Normal but significant condition.
-    COBARO_LOG_NOTICE,
-
-    /// @var Informational.
-    COBARO_LOG_INFO,
-
-    /// @var Debug-level messages.
-    COBARO_LOG_DEBUG,
-
-    /// @var Iterator useful.
-    COBARO_LOG_LEVELS_COUNT
+    COBARO_LOG_EMERG = 0,  ///< System is unusable.
+    COBARO_LOG_ALERT,  ///< Action must be taken immediately.
+    COBARO_LOG_CRIT,  ///< Critical conditions.
+    COBARO_LOG_ERR,  ///< Error conditions.
+    COBARO_LOG_WARNING,  ///< Warning conditions.
+    COBARO_LOG_NOTICE,  ///< Normal but significant condition.
+    COBARO_LOG_INFO,  ///< Informational.
+    COBARO_LOG_DEBUG,  ///< Debug-level messages.
+    COBARO_LOG_LEVELS_COUNT  ///< Iterator useful.
 };
 
 
@@ -74,13 +50,13 @@ enum cobaro_log_levels {
 
 /// Log information structure.
 ///
-/// Can carry a log code, plus COBARO_LOG_PARAM_MAX parameters of
+/// Can carry a log code, plus @ref COBARO_LOG_PARAM_MAX parameters of
 /// variant types.
-typedef struct cobaro_log {
-    /// Header.
-    ///
-    /// For use in identifying this object as a log message, when
-    /// multiple structure types are eg. delivered via the same queue.
+struct cobaro_log {
+
+    /// Opaque header for use in identifying this object as a log
+    /// message, when multiple structure types are eg. delivered via
+    /// the same queue.
     uint32_t id;
 
     /// Log code.  Zero is successful.
@@ -89,7 +65,7 @@ typedef struct cobaro_log {
     /// Queue.
     struct cobaro_log *next;
 
-    /// Log level, from cobaro_log_levels enumeration.
+    /// Log level, from @ref cobaro_log_levels enumeration.
     uint8_t level;
 
     // 3 spare bytes here.
@@ -100,7 +76,7 @@ typedef struct cobaro_log {
     /// Array of parameters relevant to this log.
     struct {
         /// Value type discriminant.  Legal values are defined in the
-        /// cobaro_log_types enumeration.
+        /// @ref cobaro_log_types enumeration.
         uint8_t type;
 
         // 7 spare bytes here.
@@ -121,58 +97,67 @@ typedef struct cobaro_log {
         };
     } p[COBARO_LOG_PARAM_MAX];
 
-} *cobaro_log_t;
+};
 
 // log->code       is the numeric identifier for a specific log.
 // log->p[0].type  is the type of the value.
 // log->p[0].s     is a reference to a string value.
 
 
-/// Log handle type.
+/// Log structure pointer type.
+typedef struct cobaro_log *cobaro_log_t;
+
+/// Opaque log handle type.
 typedef struct cobaro_loghandle *cobaro_loghandle_t;
 
 
 /// Initialize the logging infrastructure
 ///
-/// Must be called once before logging
-/// Bye default logging is to stdout at level LOG_INFO
+/// Must be called once before logging.  By default logging is to
+/// stdout at level LOG_INFO.
 ///
 /// @param[in] messages
-///     array of message format strings. See test/messages.[ch] for examples
+///    Array of message format strings. See test/messages.[ch] for
+///    examples.
 ///
 /// @returns
-///     Valid log handle on success, NULL on failure
+///    Valid log handle on success, @c NULL on failure.
 cobaro_loghandle_t cobaro_log_init(char **messages);
 
-/// Set the message catalog in use (in case you want to change language)
+/// Set the message catalog in use (in case you want to change language).
 ///
 /// @param[in] lh
-///     loghandle to set emssages catalog for
+///    Log handle to set messages catalog for.
 ///
 /// @param[in] messages
-///     array of message format strings. See test/messages.[ch] for examples
+///    Array of message format strings. See test/messages.[ch] for
+///    examples.
 ///
 /// @returns
-///     Valid log handle on success, NULL on failure
+///    Valid log handle on success, @c NULL on failure.
 void cobaro_log_messages_set(cobaro_loghandle_t lh, char **messages);
 
-/// Finalize the logging infrastructure
+/// Finalize the logging infrastructure.
 ///
 /// @param[in] lh
-///     loghandle to clean up
-///
-/// Cleans up logging before program exit
+///    Log handle to clean up.
 void cobaro_log_fini(cobaro_loghandle_t lh);
 
-/// Claim a log_t. At this point you have control over it's memory.
+/// Acquire a log structure from the handle's free list.
+///
+/// The log handle maintains set of log structures that can be passed
+/// between threads to forward a log message for reporting.  This
+/// function acquires a log structure from this free list.  On return,
+/// you have control over its memory.
 ///
 /// @param[in] lh
-///     loghandle to fetch from
+///    Log handle to fetch from.
 ///
 /// @returns
-///    Pointer to log object on success
-///    NOTE: This may return NULL in which case you are unable to log
-///          temporarily. This is to ensure we never block and is by design.
+///    Pointer to log structure on success.  @c NULL if no log
+///    structures are available from the handle.  This means that the
+///    logging system is congested, and the caller should simply
+///    continue its work without logging.
 cobaro_log_t cobaro_log_claim(cobaro_loghandle_t lh);
 
 /// Helper function for setting a string parameter.
@@ -182,8 +167,10 @@ cobaro_log_t cobaro_log_claim(cobaro_loghandle_t lh);
 ///
 /// @param[in] log
 ///    Log structure to populate.
+///
 /// @param[in] argnum
 ///    Argument number (as in, %n, being array index + 1).
+///
 /// @param[in] source
 ///    C-string to copy to log structure's parameters.
 void cobaro_log_set_string(cobaro_log_t log, int argnum, const char *source);
@@ -192,8 +179,10 @@ void cobaro_log_set_string(cobaro_log_t log, int argnum, const char *source);
 ///
 /// @param[in] log
 ///    Log structure to populate.
+///
 /// @param[in] argnum
 ///    Argument number (as in, %n, being array index + 1).
+///
 /// @param[in] source
 ///    Integer value to set.
 void cobaro_log_set_integer(cobaro_log_t log, int argnum, int64_t source);
@@ -202,8 +191,10 @@ void cobaro_log_set_integer(cobaro_log_t log, int argnum, int64_t source);
 ///
 /// @param[in] log
 ///    Log structure to populate.
+///
 /// @param[in] argnum
 ///    Argument number (as in, %n, being array index + 1).
+///
 /// @param[in] source
 ///    Double/real value to set.
 void cobaro_log_set_double(cobaro_log_t log, int argnum, double source);
@@ -212,8 +203,10 @@ void cobaro_log_set_double(cobaro_log_t log, int argnum, double source);
 ///
 /// @param[in] log
 ///    Log structure to populate.
+///
 /// @param[in] argnum
 ///    Argument number (as in, %n, being array index + 1).
+///
 /// @param[in] source
 ///    IPv4 address, in **host** byte order.
 void cobaro_log_set_ipv4(cobaro_log_t log, int argnum, uint32_t source);
@@ -221,138 +214,153 @@ void cobaro_log_set_ipv4(cobaro_log_t log, int argnum, uint32_t source);
 /// Publish a log_t relinqushing its memory.
 ///
 /// @param[in] lh
-///     loghandle to publish to
+///    Log handle to publish to.
 ///
 /// @param[in] log
 ///    Pointer to log object. This relinquishes control of the memory.
 void cobaro_log_publish(cobaro_loghandle_t lh, cobaro_log_t log);
 
-/// Receive a log_t if available.
+/// Receive a cobaro_log_t if available.
 ///
 /// @param[in] lh
-///     loghandle to receive a log from
+///    Log handle to receive a log from.
 ///
 /// @returns
-///     valid log_t or NULL if nothing waiting
+///    @c NULL if nothing waiting, otherwise pointer to cobaro_log
+///    structure to be processed.
 cobaro_log_t cobaro_log_next(cobaro_loghandle_t lh);
 
-/// Return a log_t to the free list
+/// Return a log structure to the free list.
 ///
 /// @param[in] lh
-///     loghandle to receive a log from
+///    Log handle to receive a log from.
 ///
 /// @param[in] log
-///    Pointer to log to be returned.
+///    Pointer to log structure to be returned to free list.
 void cobaro_log_return(cobaro_loghandle_t lh, cobaro_log_t log);
 
-/// Emit a log to the loghandle's default destination
+/// Emit a log to the loghandle's default destination.
 ///
 /// Only the first 1024 bytes of a log message will be written. If
-/// that is insufficient then use cobaro_log_to_string() and log directly.
+/// that is insufficient then use cobaro_log_to_string() and log
+/// directly.
 ///
 /// @param[in] lh
-///     loghandle to log with
+///    Log handle to log with.
 ///
 /// @param[in] log
-///    Pointer to log object
+///    Pointer to log structure.
+///
 /// @returns
-///    true on success, false on any failure
+///    @c true on success, @c false on any failure.
 bool cobaro_log(cobaro_loghandle_t lh, cobaro_log_t log);
 
 /// Log a message to syslog.
 ///
-/// This assumes an open syslogger connection.
+/// This call assumes your application has established and configured
+/// a syslog session using openlog(3).
 ///
 /// Only the first 1024 bytes of a log message will be written. If
-/// that is insufficient then use cobaro_log_to_string() and log directly.
+/// that is insufficient then use cobaro_log_to_string() and a
+/// suitable buffer and call syslog(3) directly.
 ///
 /// @param[in] lh
-///     loghandle to receive a log from
+///    Log handle to receive a log from.
 ///
 /// @param[in] log
-///    log data
-///
-/// @returns
-///     void
+///    Log data.
 void cobaro_log_to_syslog(cobaro_loghandle_t lh, cobaro_log_t log);
 
-/// Log a message to file
+/// Log a message to file.
+///
+/// Write the formatted log message, prepended with a timestamp, to
+/// the specified file.  The timestamp uses the local timezone, and
+/// microsecond precision.
 ///
 /// Only the first 1024 bytes of a log message will be written. If
-/// that is insufficient then use cobaro_log_to_string() and log directly.
+/// that is insufficient then use cobaro_log_to_string() and a
+/// suitable buffer and call syslog(3) directly.
 ///
 /// @param[in] lh
-///     loghandle in use
+///    Log handle in use.
 ///
 /// @param[in] log
-///    log to print
+///    Log to write.
 ///
 /// @param[in] f
-///    filehandle to print to
+///    File handle to write to, eg. stdout, stderr, or any other FILE
+///    pointer.
 ///
 /// @returns
-///     -1 on failure
-///     0 will b returned if nothing was written (loglevel too high)
-///     Otherwise the number of chracters written.
-///     errno will be ENOSPC if the formatted log message (including
-///     timestamp) is greater than 1024 bytes
-///     See fpritnf() for other errno failures
+///    If successful, returns the number of characters written to the
+///    file.  If nothing was written (because the configured log level
+///    is too high), it returns zero.
+///
+///    If there was an error, -1 is returned and an error code is
+///    available from @c errno.  @c ENOSPC indicates that the
+///    formatted message (including timestamp) exceeded 1024 bytes.
+///    See fprintf(3) and write(2) for other possible @c errno values.
 int cobaro_log_to_file(cobaro_loghandle_t lh, cobaro_log_t log, FILE *f);
 
-/// Log a message to a string
+/// Log a message to a string.
 /// 
-/// The function will not write more than s_len bytes and will always
-/// NULL terminate the string even if that means truncating s
+/// The function will not write more than @p buflen bytes and will
+/// always NUL-terminate the string even if that means truncating the
+/// formatted message.
 ///
 /// @param[in] lh
-///     loghandle in use
+///    Log handle in use.
 ///
 /// @param[in] log
-///    log data
+///    Log data.
 ///
-/// @param[in,out] s
-///     pre-allocated string for messages to be formatted in to
+/// @param[in,out] buffer
+///    Write the formatted log message to this character buffer.
 ///
-/// @param[in] s_len
-///    length of s
+/// @param[in] buflen
+///    Length of @p buffer in bytes.
 ///
 /// @returns
-///     number of characters that could have been written including
-///     the terminating null had space been available
+///    Number of characters that would have been written including
+///    the terminating NUL had space been available.
+///
+///    If this value is more than 1024, @p buffer contains a
+///    truncated, but safely terminated, message.
 int cobaro_log_to_string(cobaro_loghandle_t lh, cobaro_log_t log,
-                          char *s, size_t s_len);
+			 char *buffer, size_t buflen);
 
-/// Set the log level below which we should ignore logs
+/// Set the log level below which we should ignore logs.
 ///
 /// @param[in] lh
-///     loghandle in use
+///     Log handle in use.
 ///
 /// @param[in] level
-///    level to log below, see man (3) syslog. Default: LOG_INFO
+///    Level to log below, see syslog(3) for defined values. Default
+///    is LOG_INFO.
 ///
 /// @returns
-///    true on success, false on failure
+///    @c true on success, @c false on failure.
 bool cobaro_log_loglevel_set(cobaro_loghandle_t lh, int level);
 
-/// Set the default log destination to be a file handle
+/// Set the default log destination to be a file handle.
 ///
 /// @param[in] lh
-///     loghandle in use
+///     Log handle in use.
 ///
 /// @param[in] f
-///     filehandle to log to
+///     File handle to log to.
 ///
 /// @returns
-///    true on success, false on failure
+///    @c true on success, @c false on failure.
 bool cobaro_log_file_set(cobaro_loghandle_t lh, FILE *f);
 
-/// Set the defaulkt log destination to be syslog
+/// Set the default log destination to be syslog.
 ///
 /// Caller is responsible for calling openlog(ident, option, facility)
-/// and closelog(), see man (3) syslog
+/// and closelog(), see syslog(3).
 ///
 /// @returns
-///    true on success, false on failure
+///    @c true on success, @c false on failure.
 bool cobaro_log_syslog_set(cobaro_loghandle_t lh);
 
 
